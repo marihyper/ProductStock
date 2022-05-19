8ca898ebe81120f53ef1d36fb94d7beb151584d1
Mari Abreu <81270107+marihyper@users.noreply.github.com>
2022-05-19 19:02:00 -0300

    Delete src directory

--- src/AmountComparator.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-public class AmountComparator implements Comparator<Product> {
-    public int compare(Product n1, Product n2) {
-        return Integer.compare(n1.getAmount(), n2.getAmount());
-    }
-}
